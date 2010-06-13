3795b846ea4eed748f3a82a303282bc3cd6811cf
Stefano Gualdi <stefano.gualdi@gmail.com>
2010-06-13 13:03:44 +0200

    Version bump

--- CodemirrorGrailsPlugin.groovy
+++ CodemirrorGrailsPlugin.groovy
@@ -18,7 +18,7 @@
 
 class CodemirrorGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.2 > *"
